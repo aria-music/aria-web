2556c39309e073ad2ecbc172ccd7c72f874ae641
mnsinri <khj.mkyohei727@gmail.com>
2019-10-13 03:00:46 +0900

    add subqueue

--- src/components/footer/subQueueliist.vue
+++ src/components/footer/subQueueliist.vue
@@ -1,15 +1,72 @@
 <template>
-  <v-menu>
-    <v-btn color="primary" dark slot="activator">text</v-btn>
-    <v-list>
-      <v-list-tile v-for="item in items" :key="item.key" @click="callback">
-        <v-list-tile-title>{{ item.key }}</v-list-tile-title>
-      </v-list-tile>
-    </v-list>
+  <v-menu
+    :close-on-content-click="false"
+    transition="slide-y-reverse-transition"
+    top
+    nudge-top="70"
+  >
+    <template #activator="{ on }">
+      <v-btn
+        icon
+        v-on="on"
+      >
+        <v-icon>playlist_play</v-icon>
+      </v-btn>
+    </template>
+    <v-card
+      max-width="600"
+    >
+      <v-hover #default="{ hover }">
+        <v-img
+          class="align-end"
+          height="150px"
+          :src="nowPlaying.thumnail"
+        >
+          <v-card-text class="py-0">Playing:</v-card-text>
+          <v-row class="title pl-4 pb-3 pt-1" no-gutters>
+            <v-col cols="11" class="font-weight-medium text-truncate">
+              {{ nowPlaying.title }}
+            </v-col>
+            <v-col cols="1" class="align-self-end">
+              <v-fade-transition mode="out-in">
+                <!-- TODO functional btn -->
+                <v-btn
+                  v-show="hover"
+                  icon
+                  small
+                ><v-icon>more_vert</v-icon></v-btn>
+              </v-fade-transition>
+            </v-col>
+          </v-row>
+        </v-img>
+      </v-hover>
+
+      <ariaQueue
+        :maxHeight="maxHeight"
+      />
+
+    </v-card>
   </v-menu>
 </template>
 <script>
+import ariaQueue from '../options/queue/queue'
+
 export default {
+  components: {
+    ariaQueue,
+  },
+  data: () => ({
+    nowPlaying: {
+      title: "水瀬いのり『Inori Minase LIVE TOUR Catch the Rainbow！』ダイジェスト",
+      artist: "水瀬いのり",
+      thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg",
+    },
+  }),
+  computed: {
+    maxHeight() {
+      return window.innerHeight - 360
+    }
+  }
 
 }
 </script>
---
+++ src/components/options/queue/queue.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <v-row dense class="px-4">
+      <v-col cols="1">
+      </v-col>
+      <v-divider vertical class="my-1"></v-divider>
+      <v-col cols="7">
+        <v-list-item-title class="font-weight-medium caption py-0">Title</v-list-item-title>
+      </v-col>
+      <v-divider vertical class="my-1"></v-divider>
+    </v-row>
+    <v-divider></v-divider>
+    <v-list
+      :max-height="maxHeight"
+      min-height="200"
+      style="overflow: auto;"
+      class="py-0"
+      dense
+    >
+      <v-list-item
+        v-for="(item, index) in queue"
+        :key="index"
+      >
+        <v-row dense style="height: 40px">
+
+          <!-- thumnail -->
+          <v-col cols="1" class="py-0 my-auto">
+            <v-img
+              :src="item.thumnail"
+              eager
+              :aspect-ratio="1"
+              contain
+              height="35"
+            >
+
+            </v-img>
+          </v-col>
+
+          <!-- title -->
+          <v-col cols="7">
+            <v-list-item-content class="py-0">
+              <v-list-item-title v-text="item.title" class="text-truncate"></v-list-item-title>
+              <v-list-item-subtitle v-text="item.artist" class="text-truncate"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-col>
+
+          <!-- btns -->
+          <v-col cols="4">
+            <!-- TODO love btn -->
+            <!-- TODO playlist btn -->
+            <!-- TODO delete btn -->
+          </v-col>
+        </v-row>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+<script>
+const QUEUE = [
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+  {title: "hogehoge", artist: "fugafuga", thumnail: "https://images-na.ssl-images-amazon.com/images/I/51aYItqS1iL._AC_.jpg"},
+]
+
+export default {
+  props: {
+    maxHeight: {
+      style: String,
+      required: true,
+    }
+  },
+  data: () => ({
+
+  }),
+  computed: {
+    queue() {
+      return QUEUE
+    }
+  }
+}
+</script>
