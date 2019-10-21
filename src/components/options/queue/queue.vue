ef3e7fb9041767d89c1f164cdbd5d342e4696d4c
mnsinri <khj.mkyohei727@gmail.com>
2019-10-22 03:21:38 +0900

    Update: sm view supported

--- src/components/options/queue/queue.vue
+++ src/components/options/queue/queue.vue
@@ -2,7 +2,7 @@
   <v-list
     :max-height="maxHeight"
     :min-height="200"
-    :width="width"
+    :max-width="width"
     style="overflow: auto"
     class="py-0 queue"
     dense
@@ -12,72 +12,83 @@
         <v-list-item
           v-for="(item, index) in queue"
           :key="index"
-          class="pa-0"
           :ripple="false"
+          class="pa-0"
         >
           <v-hover #default="{ hover }">
-            <v-row
-              dense
-              class="ma-0"
-              style="height: 40px"
-              :style="{width: `${width}px`}"
-            >
+            <v-card :max-width="width" flat>
+              <v-row
+                dense
+                class="ma-0"
+                style="height: 40px"
+              >
 
-              <!-- thumnail -->
-              <v-col cols="2" class="py-0 d-flex align-center justify-start">
-                <v-img
-                  :src="item.thumbnail_small"
-                  eager
-                  height="35"
-                  max-width="70"
-                  contain
-                  @error.prevent
-                ></v-img>
-              </v-col>
+                <!-- thumnail -->
+                <v-col
+                  :cols="2"
+                  class="py-0 d-flex align-center justify-start"
+                >
+                  <v-img
+                    :src="item.thumbnail_small"
+                    eager
+                    height="35"
+                    :max-width="$vuetify.breakpoint.smAndUp ? 70 : 35"
+                    contain
+                    @error.prevent
+                  ></v-img>
+                </v-col>
 
-              <!-- title -->
-              <v-col cols="6" class="py-0 my-auto" :title="item.title">
-                <v-list-item-content class="py-0">
-                  <v-list-item-title
-                    v-text="item.entry ? item.entry.title : item.title"
-                    class="text-truncate"
-                  ></v-list-item-title>
-                  <v-list-item-subtitle
-                    v-text="item.entry ? item.entry.artist : ''"
-                    class="text-truncate"
-                  ></v-list-item-subtitle>
-                </v-list-item-content>
-              </v-col>
+                <!-- title -->
+                <v-col
+                  :cols="$vuetify.breakpoint.smAndUp ? 6 : 10"
+                  class="py-0 my-auto"
+                  :title="item.title"
+                >
+                  <v-list-item-content class="py-0">
+                    <v-list-item-title
+                      v-text="item.entry ? item.entry.title : item.title"
+                      class="text-truncate"
+                    ></v-list-item-title>
+                    <v-list-item-subtitle
+                      v-text="item.entry ? item.entry.artist : ''"
+                      class="text-truncate"
+                    ></v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-col>
 
-              <!-- btns -->
-              <v-col cols="4" class="py-0 d-flex align-center">
-                <!-- love btn -->
-                <lovebtn
-                  v-show="hover"
-                  small
-                  :uri="item.uri"
-                  :isLoved="false"
-                />
-                  <!-- v-show="item.is_liked || hover"
-                  :isLoved="item.is_liked" -->
-
-                <div v-show="hover">
-                  <!-- delete btn -->
-                  <deletebtn
+                <!-- btns -->
+                <v-col
+                  cols="4"
+                  class="py-0 d-flex align-center"
+                  v-if="$vuetify.breakpoint.smAndUp"
+                >
+                  <!-- love btn -->
+                  <lovebtn
+                    v-show="hover"
                     small
-                    where="queue"
                     :uri="item.uri"
-                    :index="index"
+                    :isLoved="false"
                   />
+                    <!-- v-show="item.is_liked || hover"
+                    :isLoved="item.is_liked" -->
 
-                  <!-- info btn -->
-                  <infobtn
-                    v-if="$vuetify.breakpoint.smAndUp"
-                    small
-                  />
-                </div>
-              </v-col>
-            </v-row>
+                  <div v-show="hover">
+                    <!-- delete btn -->
+                    <deletebtn
+                      small
+                      where="queue"
+                      :uri="item.uri"
+                      :index="index"
+                    />
+
+                    <!-- info btn -->
+                    <infobtn
+                      small
+                    />
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card>
           </v-hover>
         </v-list-item>
       </v-fade-transition>
--- src/components/subQueue.vue
+++ src/components/subQueue.vue
@@ -1,29 +1,30 @@
 <template>
-  <v-card
-    max-width="500"
-  >
+  <v-card>
     <v-hover #default="{ hover }">
       <v-img
         class="align-end"
         height="150px"
-        max-width="500px"
+        :max-width="$vuetify.breakpoint.xs ? 250 : 500"
         :src="playingData.thumbnail"
         gradient="rgba(100,115,201,.33), rgba(25,32,72,.7)"
       >
         <v-card-text class="py-0 font-weight-midium white--text">Playing:</v-card-text>
         <v-row class="title pl-4 pb-3 pt-1" no-gutters>
           <v-col
-            cols="11"
+            :cols="$vuetify.breakpoint.xs ? 10 : 11"
             class="text-truncate font-weight-midium white--text"
             :title="playingTitle"
           >
             {{ playingTitle }}
           </v-col>
-          <v-col cols="1" class="align-self-end">
+          <v-col
+            :cols="$vuetify.breakpoint.xs ? 2 : 1"
+            class="align-self-end"
+          >
             <!-- functional btn -->
             <funcbtn
-              :show="hover"
-              hover
+              :show="hover || $vuetify.breakpoint.xs"
+              :hover="$vuetify.breakpoint.smAndUp"
               white
               addList
               skip
@@ -36,22 +37,30 @@
     </v-hover>
 
     <v-row dense class="ma-0">
-      <v-col cols="2"></v-col>
-      <v-col cols="6" class="d-flex align-center px-0">
+      <v-col
+        offset="2"
+        :cols="$vuetify.breakpoint.xs ? 10 :6"
+        class="d-flex align-center px-0"
+      >
         <v-divider vertical></v-divider>
         <v-list-item-title class="font-weight-medium caption py-0 ml-1">Title</v-list-item-title>
       </v-col>
-      <v-col cols="4" class="d-flex align-center">
+      <v-col
+        cols="4"
+        class="d-flex align-center"
+        v-if="$vuetify.breakpoint.smAndUp"
+      >
         <v-divider vertical></v-divider>
         <v-icon small class="ml-2">favorite_border</v-icon>
         <v-icon small class="ml-5">delete_outline</v-icon>
-        <v-icon small class="ml-5" v-if="$vuetify.breakpoint.smAndUp">info</v-icon>
+        <v-icon small class="ml-5">info</v-icon>
       </v-col>
     </v-row>
     <v-divider></v-divider>
     <div style="height: 100%">
       <ariaQueue
         :maxHeight="maxHeight"
+        :width="$vuetify.breakpoint.xs ? 250 : 500"
       />
     </div>
   </v-card>
