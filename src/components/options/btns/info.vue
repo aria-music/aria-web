0586499c4df1a78660e1c29469c3dd3a5ed404f2
mnsinri <khj.mkyohei727@gmail.com>
2019-12-02 02:10:07 +0900

    update : seeable pure thumbnail

--- src/components/options/btns/info.vue
+++ src/components/options/btns/info.vue
@@ -17,21 +17,23 @@
       <v-card class="ml-2">
         <v-container grid-list-xs>
           <v-row>
-            <v-col :cols="isXs ? 12 : 6">
+            <v-col cols="12">
               <v-card
                 class="pa-2 mx-auto"
                 elevation="6"
-                max-height="300"
-                max-width="300"
+                max-height="400"
+                max-width="400"
               >
                 <v-img
                   :src="pure_thumbnail"
-                  :aspect-ratio="1"
                   @error="error"
+                  contain
+                  max-height="350"
+                  max-width="400"
                 ></v-img>
               </v-card>
             </v-col>
-            <v-col :cols="isXs ? 12 : 6">
+            <v-col cols="10" offset="1">
               <v-card elevation="6" class="mx-auto">
                 <v-card-text>
                   <div class="mb-1">
