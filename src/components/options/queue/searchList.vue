9e72e846e7e028f6ca99b16e4eaaff91ecfff8f2
mnsinri <khj.mkyohei727@gmail.com>
2019-12-03 22:34:37 +0900

    add : grabble scroll bar

--- src/components/options/queue/searchList.vue
+++ src/components/options/queue/searchList.vue
@@ -3,61 +3,60 @@
     :max-height="maxHeight"
     :max-width="maxWidth"
     style="overflow: auto"
-    class="scroller"
     >
-      <v-list-item-group>
-        <v-fade-transition group>
-          <v-list-item
-            v-for="(item, index) in searchData"
-            :key="index"
-            :ripple="false"
-          >
-            <v-lazy
-              style="width: 100%"
-              min-height="50"
-              :options="{threshold: .3}"
+      <perfect-scrollbar>
+        <v-list-item-group>
+          <v-fade-transition group>
+            <v-list-item
+              v-for="(item, index) in searchData"
+              :key="index"
+              :ripple="false"
             >
-              <v-hover #default="{ hover }">
-                  <v-row
-                    dense
-                  >
-                    <!-- thumnail -->
-                    <v-col
-                      :cols="2"
-                      class="py-0 d-flex align-center"
-                    >
-                      <imgObj
-                        :src="item.thumbnail_small"
-                        :height="45"
-                        :maxWidth="70"
-                        eager
-                        contain
-                      />
-                    </v-col>
-                    <!-- title -->
-                    <v-col
-                      :cols="6"
-                    >
-                      <v-list-item-content class="py-0">
-                        <v-list-item-title
-                          v-text="item.entry ? item.entry.title : item.title"
-                          class="text-truncate"
-                        ></v-list-item-title>
-                        <v-list-item-subtitle
-                          v-text="item.entry ? item.entry.artist : ''"
-                          class="text-truncate"
-                        ></v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-col>
-                    <!-- <v-col cols="">
+              <v-lazy
+                style="width: 100%"
+                min-height="50"
+                :options="{threshold: .3}"
+              >
+                <v-hover #default="{ hover }">
+                    <v-row dense>
+                      <!-- thumnail -->
+                      <v-col
+                        :cols="2"
+                        class="py-0 d-flex align-center"
+                      >
+                        <imgObj
+                          :src="item.thumbnail_small"
+                          :height="45"
+                          :maxWidth="70"
+                          eager
+                          contain
+                        />
+                      </v-col>
+                      <!-- title -->
+                      <v-col
+                        :cols="6"
+                      >
+                        <v-list-item-content class="py-0">
+                          <v-list-item-title
+                            v-text="item.entry ? item.entry.title : item.title"
+                            class="text-truncate"
+                          ></v-list-item-title>
+                          <v-list-item-subtitle
+                            v-text="item.entry ? item.entry.artist : ''"
+                            class="text-truncate"
+                          ></v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-col>
+                      <!-- <v-col cols="">
 
-                    </v-col> -->
-                  </v-row>
-              </v-hover>
-            </v-lazy>
-          </v-list-item>
-        </v-fade-transition>
-      </v-list-item-group>
+                      </v-col> -->
+                    </v-row>
+                </v-hover>
+              </v-lazy>
+            </v-list-item>
+          </v-fade-transition>
+        </v-list-item-group>
+      </perfect-scrollbar>
     </v-list>
 </template>
 <script>
@@ -81,4 +80,3 @@ export default {
   }
 }
 </script>
-<style lang="scss" src="@/components/options/scss/scroller.scss">
