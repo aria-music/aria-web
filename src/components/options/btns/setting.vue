213c680d2ae60e0211e93a9b99dc22bba1705956
Sarisia <sarisiaice@gmail.com>
2019-12-19 01:03:54 +0900

    easter egg

--- src/components/options/btns/setting.vue
+++ src/components/options/btns/setting.vue
@@ -4,6 +4,7 @@
 		bottom
 		left
 		nudge-bottom="50"
+		:close-on-content-click="shouldClose"
 	>
 		<template #activator="{ on }">
 			<v-btn
@@ -64,9 +65,13 @@ export default {
 		settingItems: settingItems,
 		subheader: "KANARI VERY FAST",
 		selectTheme: false,
+		shouldClose: true,
+		pianoTimer: null,
+		pianoCount: 0
 	}),
 	methods: {
 		setting(contents){
+			this.shouldClose = true
 			switch(contents){
 				case "theme":
 					this.changeThemeColor()
@@ -75,10 +80,27 @@ export default {
 					this.initAudioContext()
 					break
 				case "no":
+					this.shouldClose = false
+					if (this.pianoTimer) {
+						++this.pianoCount
+						if (this.pianoCount > 3) {
+							this.resetPianoCount()
+							this.$router.push({ name: "piano" })
+						}
+					} else {
+						this.pianoTimer = setTimeout(() => {
+							this.resetPianoCount()
+						}, 1500);
+					}
 					break
 				default:
 					break
 			}
+		},
+		resetPianoCount() {
+			this.pianoCount = 0
+			clearTimeout(this.pianoTimer)
+			this.pianoTimer = null
 		},
 		changeThemeColor(){
 			this.selectTheme = !this.selectTheme
