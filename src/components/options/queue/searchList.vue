c55e8f98cf8c99a98e14171cc5918e5f4ffabdcd
mnsinri <khj.mkyohei727@gmail.com>
2020-01-09 01:09:19 +0900

    delete: hover

--- src/components/options/queue/searchList.vue
+++ src/components/options/queue/searchList.vue
@@ -18,41 +18,39 @@
                 min-height="50"
                 :options="{threshold: .3}"
               >
-                <v-hover #default="{ hover }">
-                    <v-row dense>
-                      <!-- thumnail -->
-                      <v-col
-                        :cols="2"
-                        class="py-0 d-flex align-center"
-                      >
-                        <imgObj
-                          :src="item.thumbnail_small"
-                          :height="45"
-                          :maxWidth="70"
-                          eager
-                          contain
-                        />
-                      </v-col>
-                      <!-- title -->
-                      <v-col
-                        :cols="6"
-                      >
-                        <v-list-item-content class="py-0">
-                          <v-list-item-title
-                            v-text="item.entry ? item.entry.title : item.title"
-                            class="text-truncate"
-                          ></v-list-item-title>
-                          <v-list-item-subtitle
-                            v-text="item.entry ? item.entry.artist : ''"
-                            class="text-truncate"
-                          ></v-list-item-subtitle>
-                        </v-list-item-content>
-                      </v-col>
-                      <!-- <v-col cols="">
+                <v-row dense>
+                  <!-- thumnail -->
+                  <v-col
+                    :cols="2"
+                    class="py-0 d-flex align-center"
+                  >
+                    <imgObj
+                      :src="item.thumbnail_small"
+                      :height="45"
+                      :maxWidth="70"
+                      eager
+                      contain
+                    />
+                  </v-col>
+                  <!-- title -->
+                  <v-col
+                    :cols="6"
+                  >
+                    <v-list-item-content class="py-0">
+                      <v-list-item-title
+                        v-text="item.entry ? item.entry.title : item.title"
+                        class="text-truncate"
+                      ></v-list-item-title>
+                      <v-list-item-subtitle
+                        v-text="item.entry ? item.entry.artist : ''"
+                        class="text-truncate"
+                      ></v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-col>
+                  <!-- <v-col cols="">
 
-                      </v-col> -->
-                    </v-row>
-                </v-hover>
+                  </v-col> -->
+                </v-row>
               </v-lazy>
             </v-list-item>
           </v-fade-transition>
